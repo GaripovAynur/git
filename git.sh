013377745c2ec73faf5fd74b1abb0bb5b20764dc
GaripovAynur <kuyan911@gmail.com>
2021-08-02 15:40:35 +0300

    Update and rename git to git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,33 @@
+#####Git global setup####
+git config --global user.name "Айнур Гарипов"
+git config --global user.email "kuyan911@gmail.com"
+
+####Create a new repository####
+git clone git@gitlab.com:kuyan911/test.git
+cd test
+git switch -c main
+touch README.md
+git add README.md
+git commit -m "add README"
+git push -u origin main
+
+#####Push an existing folder####
+cd existing_folder
+git init --initial-branch=main
+git remote add origin git@gitlab.com:kuyan911/test.git
+git add .
+git commit -m "Initial commit"
+git push -u origin main
+
+####Push an existing Git repository###
+cd existing_repo
+git remote rename origin old-origin
+git remote add origin git@gitlab.com:kuyan911/test.git
+git push -u origin --all
+git push -u origin --tags
+
+
+############
 git init .              # Можно  git init etc\myproject 
 git add .               # Команда git add добавляет содержимое рабочей директории в индекс (staging area) для последующего коммита
 git add <file> <file>   
